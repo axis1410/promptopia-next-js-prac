a9710594679bd035a29a27d74109e408ea5e725d
axis.1410 <adityasingh14102001@gmail.com>
2023-06-21 01:30:29 +0530

    Added profile page and feed component

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,6 +1,70 @@
-const PromptCard = () => {
-  return (
-    <div>PromptCard</div>
-  )
-}
-export default PromptCard
+'use client';
+
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+	const [copied, setCopied] = useState('');
+	const { data: session } = useSession();
+	const pathName = usePathname();
+	const router = useRouter();
+
+	const handleCopy = () => {
+		setCopied(post.prompt);
+		navigator.clipboard.writeText(post.prompt);
+		setTimeout(() => setCopied(''), 3000);
+	};
+
+	return (
+		<div className='prompt_card'>
+			<div className='flex justify-between items-start gap-5'>
+				<div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+					<Image
+						src={post.creator.image}
+						alt='user_image'
+						width={40}
+						height={40}
+						className='rounded-full object-contain'
+					/>
+					<div className='flex flex-col'>
+						<h3 className='font-satoshi font-semibold text-gray-900'>
+							{post.creator.username}
+							<p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
+						</h3>
+					</div>
+				</div>
+				<div className='copy_btn' onClick={handleCopy}>
+					<Image
+						src={copied === post.prompt ? 'assets/icons/tick.svg' : 'assets/icons/copy.svg'}
+						width={15}
+						height={15}
+						alt={copied === post.prompt ? 'copied_to_clipboard' : 'copy_to_clipboard'}
+					/>
+				</div>
+			</div>
+			<p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+			<p
+				className='font-inter text-sm blue_gradient cursor-pointer'
+				onClick={() => handleTagClick && handleTagClick(post.tag)}
+			>
+				{post.tag}
+			</p>
+			{session?.user.id === post.creator._id && pathName === '/profile' && (
+				<div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+					<p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>
+						Edit
+					</p>
+					<p
+						className='font-inter text-sm orange_gradient cursor-pointer'
+						onClick={handleDelete}
+					>
+						Delete
+					</p>
+				</div>
+			)}
+		</div>
+	);
+};
+export default PromptCard;
