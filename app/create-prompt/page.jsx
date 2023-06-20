7bcf0383c0cca2da1ad9b52f5df74566c8f0af8a
axis.1410 <adityasingh14102001@gmail.com>
2023-06-21 00:20:05 +0530

    Implemented post prompt functionality

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import Form from '@components/Form';
+
+const CreatePrompt = () => {
+	const router = useRouter();
+	const { data: session, status } = useSession();
+
+	const [submitting, setSubmitting] = useState(false);
+	const [post, setPost] = useState({
+		prompt: '',
+		tag: '',
+	});
+
+	const createPrompt = async (e) => {
+		e.preventDefault();
+		setSubmitting(true);
+
+		try {
+			if (status === 'authenticated') {
+				const response = await fetch('/api/prompt/new', {
+					method: 'POST',
+					body: JSON.stringify({
+						prompt: post.prompt,
+						userId: session.user.id,
+						tag: post.tag,
+					}),
+				});
+
+				if (response.ok) {
+					router.push('/');
+				}
+			} else {
+				// User is not authenticated, redirect to sign-in page
+				await signIn();
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setSubmitting(false);
+		}
+	};
+
+	return (
+		<Form
+			type='Create'
+			post={post}
+			setPost={setPost}
+			submitting={submitting}
+			handleSubmit={createPrompt}
+		/>
+	);
+};
+
+export default CreatePrompt;
